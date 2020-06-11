2d63f7ba90c84741ff9c7410251c12850554ee4b
Kegan Ferreira <keganferreira@outlook.com>
2020-06-11 12:25:52 +1000

    changed fdm to lutoke

--- backend/src/main/java/com/fdm/pond/repository/MTCRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fdm.pond.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.fdm.pond.model.opportunity.ManagersToClients;
-
-public interface MTCRepository extends PagingAndSortingRepository<ManagersToClients, Long>{
-
-}
--- backend/src/main/java/com/fdm/pond/repository/SkillRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fdm.pond.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.fdm.pond.model.employee.Skill;
-
-public interface SkillRepository extends PagingAndSortingRepository<Skill, String>{
-}
